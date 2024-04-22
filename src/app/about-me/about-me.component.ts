d46218a50e67cf2fecd4d8671e41d82cf1bbbf9b
Mohamed Ali <mohamed.ali@kovai.co>
2024-04-22 23:15:03 +0530

    changers in about page

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
-  styleUrls: ['./about-me.component.scss']
+  styleUrls: ['./about-me.component.scss'],
 })
-export class AboutMeComponent implements OnInit {
+export class AboutMeComponent implements AfterViewInit {
+  @ViewChild('borderBack') borderBack!: ElementRef;
+  @ViewChild('borderFront') borderFront!: ElementRef;
+  isHovered: boolean = false;
+  currentImageUrlIndex = 0;
+  imageUrls = [
+    '../../assets/image/About/1.jpg',
+    '../../assets/image/About/2.jpg',
+    '../../assets/image/About/3.jpg',
+    '../../assets/image/About/4.png',
+  ];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private renderer: Renderer2) {}
+  toggleSpin() {
+    // this.isHovered = !this.isHovered;
+    this.isHovered = false;
+  }
+  ngAfterViewInit() {
+    setInterval(() => {
+      this.rotateImage();
+    }, 5000); // Rotate every 5 seconds (adjust as needed)
   }
 
+  rotateImage() {
+    this.currentImageUrlIndex =
+      (this.currentImageUrlIndex + 1) % this.imageUrls.length;
+  }
 }
